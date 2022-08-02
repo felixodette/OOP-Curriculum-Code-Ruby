3a82f48d9f0c7d2e2d88012a3870269eea144027
Felix Odete <felix.odette@gmail.com>
2022-08-02 08:41:04 +0300

    Implement dog and spider class

---
+++ animals.rb
@@ -0,0 +1,27 @@
+class Animal
+  def initialize(type, number_of_legs, name = "Unknown")
+    @id = Random.rand(1..1000)
+    @name = name
+    @number_of_legs = number_of_legs
+    @type = type
+  end
+
+  def id
+    @id
+  end
+
+  def type
+    @type
+  end
+
+  def number_of_legs
+    @number_of_legs
+  end
+
+  attr_accessor :name
+
+  def speak()
+    "grrrr"
+  end
+end
+
---
+++ dog.rb
@@ -0,0 +1,16 @@
+require "./animals.rb"
+
+class Dog < Animal
+  def initialize(color, name = "Unknown")
+    super("dog", 4, name)
+    @color = color
+  end
+
+  def bring_a_stick
+    "Here is your stick: ----------"
+  end
+
+  def speak
+    "woof, woof"
+  end
+end
