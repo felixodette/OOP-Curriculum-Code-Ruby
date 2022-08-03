781561c7c7d2285857916468b59009000950b2c1
Felix Odete <felix.odette@gmail.com>
2022-08-03 17:22:24 +0300

    Composition by example

--- animals.rb
+++ animals.rb
@@ -1,9 +1,13 @@
+require './remover'
+require './foods'
+
 class Animal
   def initialize(type, number_of_legs, name = "Unknown")
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
     @type = type
+    @liked_food = NoFood.new()
   end
 
   def id
@@ -22,6 +26,11 @@ class Animal
 
   def speak()
     "grrrr"
+  end
+
+  def remove_leg
+    remover = Remover.new()
+    @number_of_legs = remover.decrease(@number_of_legs)
   end
 end
 
--- dog.rb
+++ dog.rb
@@ -1,9 +1,11 @@
 require "./animals.rb"
+require './foods'
 
 class Dog < Animal
   def initialize(color, name = "Unknown")
     super("dog", 4, name)
     @color = color
+    @liked_food = DogFood.new()
   end
 
   def bring_a_stick
